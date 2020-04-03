270fe6a31a68317e344d32ef66a6eec85c865fbf
Geert Michiels <>
2020-04-03 16:56:54 +0200

    fix to update timeout immediatly when recieved via MQTT (and not after reboot)

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -278,6 +278,7 @@ void onConnectionEstablished() {
     //register for beeper
     g_mqtt.subscribe(g_mqtt.topicForPath("subscribe/auto_switchoff_seconds"), [](const String & payload) {
         g_config.setAutoSwitchOffSeconds(payload.toInt());
+        g_relayTimeout.updateTimeoutAndRestart(g_config.getAutoSwitchOffSeconds()*1000);
         g_config.commitToEEPROM();
         g_beeper.blink(1, 1, 10);
         g_mqtt.publishDeviceValues();
